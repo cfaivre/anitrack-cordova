817dea1df5ac31e7ed475fa86c461f31391549f2
Jiaan <jiaan.louw@hetzner.co.za>
2017-01-16 18:43:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# AniTracker
+
+Cordova application.
+
+## Developement environment
+
+This project requires **[npm](https://www.npmjs.com/)** to be installed.
+
+Install cordova if you have not already.
+
+```bash
+npm install cordova -g
+```
+
+### Dependancies
+
+The android platform requires the [Java SDK aka JDK](http://www.oracle.com/technetwork/java/javase/downloads/index.html) to be installed and PATHS configured.
+
+Install the required cordova plugins & platforms.
+```bash
+npm install
+```
+
+### Build
+
+```bash
+npm run build:android
+```
+
+```bash
+npm run build:ios
+```
---
+++ install.js
@@ -0,0 +1,32 @@
+const plugins = require('./plugins/fetch.json');
+const platforms = require('./platforms/platforms.json');
+const exec = require('child_process').exec;
+
+console.log('\n\rRunning jobs...\n\r');
+
+var jobs = [];
+
+for (var key in plugins) {
+	var src = plugins[key].source.id;
+  var cmd = `cordova plugin add ${src}`;
+  jobs.push(cmd);
+}
+
+for (var platform in platforms) {
+	var version = platforms[platform];
+	var cmd = `cordova platform add ${platform}@${version}`;
+	jobs.push(cmd);
+}
+
+function runNextJob() {
+	var job = jobs.pop();
+	if (!job) return console.log('Done.');
+	console.log(job);
+	exec(job, (error, stdout, stderr) => {
+  	stdout && console.log(stdout);
+  	stderr && console.log(stderr);
+  	runNextJob();
+  });
+}
+
+runNextJob();
