99ef49a3150dcebbb4ef1cc750f1c8ca6ec716f8
Jiaan <jiaan.louw@hetzner.co.za>
2017-01-25 23:53:16 +0200

    added animal details view

--- www/js/app.js
+++ www/js/app.js
@@ -14,7 +14,12 @@ var app = {
     this.displayContent(display);
   },
 
-  displayContent: function(display) {
+  clickedAnimal: function(el) {
+    var id = el.getAttribute('_id');
+    this.displayContent('animal_details', [id]);
+  },
+
+  displayContent: function(display, args) {
     var displays = document.querySelectorAll('#content > div');
     var i = displays.length;
     var selected = null;
@@ -26,7 +31,12 @@ var app = {
         displays[i].classList.remove('selected');
       }
     }
-    this.component[display] && this.component[display].render(selected);
+    if (this.component[display]) {
+      args = args || [];
+      args.unshift(selected);
+      var component = this.component[display];
+      component.render.apply(component, args);
+    }
   }
 
 };
@@ -115,7 +125,7 @@ app.component.animals = {
   },
 
   createElements: function(result) {
-    var data = result;
+    var data = result.data;
     for (var i = 0; i < data.length; i++) {
       var obj = data[i];
       var node = this.template.cloneNode(true);
@@ -133,6 +143,74 @@ app.component.animals = {
       node.classList.remove('template');
       this.element.appendChild(node);
     }
+  }
+
+};
+
+app.component.animal_details = {
+
+  element: null,
+
+  template: null,
+
+  render: function(el, id) {
+    this.default(el);
+    this.fetchAnimal(id, this.displayDetails.bind(this));
+  },
+
+  default: function(el) {
+    this.element = this.element || el;
+    this.template = this.template || el.querySelector('.template').cloneNode(true);
+    this.element.querySelector('.list').innerHTML = '';
+  },
+
+  fetchAnimal: function(id, callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var result = JSON.parse(this.responseText);
+        callback(result);
+      }
+    };
+    xhttp.open("GET", "http://anitrack-api.faivre.co.za/animal/view?id=" + id, true);
+    xhttp.send();
+  },
+
+  displayDetails: function(result) {
+    var data = result.data;
+    var databindings = this.element.querySelectorAll('.details *');
+    databindings.forEach(function(el) {
+      if (el.hasAttribute('attr')) {
+        var key = el.getAttribute('attr');
+        el.setAttribute(key, data[key]);
+      }
+      if (el.hasAttribute('content')) {
+        var key = el.getAttribute('content');
+        el.innerHTML = data[key];
+      }
+    });
+    this.createListElements(data);
+  },
+
+  createListElements: function(data) {
+    var events = data.life_events;
+    for (var i = 0; i < events.length; i++) {
+      var obj = events[i];
+      var node = this.template.cloneNode(true);
+      var databindings = node.querySelectorAll('*');
+      databindings.forEach(function(el) {
+        if (el.hasAttribute('attr')) {
+          var key = el.getAttribute('attr');
+          el.setAttribute(key, obj[key]);
+        }
+        if (el.hasAttribute('content')) {
+          var key = el.getAttribute('content');
+          el.innerHTML = obj[key];
+        }
+      });
+    }
+    node.classList.remove('template');
+    this.element.querySelector('.list').appendChild(node);
   }
 
 };
